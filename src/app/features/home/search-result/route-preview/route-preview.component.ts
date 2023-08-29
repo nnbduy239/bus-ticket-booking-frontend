bbcbae2f086362a9a3cd90741ed052719de2fef0
DESKTOP-L4FTB5J\Win 10 <nnbduy239@gmail.com>
2023-08-30 06:27:54 +0700

    feat(SearchResult) Add list route and route preivew -Update the templates and styles

---
+++ src/app/features/home/search-result/route-preview/route-preview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-preview',
+  templateUrl: './route-preview.component.html',
+  styleUrls: ['./route-preview.component.scss']
+})
+export class RoutePreviewComponent {
+
+}
